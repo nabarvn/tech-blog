07cd229a514391d73f517fa6587046e39a62d1a8
Nabarun <nicknabarun@gmail.com>
2022-11-21 21:48:35 +0530

    Import components and gql query

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,39 @@
 import Head from "next/head";
+import { PostCard, CategoriesWidget, PostsWidget } from "../components";
+import FeaturedPosts from "../sections/FeaturedPosts";
+import { getPosts } from "../services";
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>My Blog</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>Hello World</h1>
+      <FeaturedPosts />
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='col-span-1 lg:col-span-8'>
+          {posts.map((post) => (
+            <PostCard key={post.node.title} post={post.node} />
+          ))}
+        </div>
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relative lg:sticky top-8'>
+            <PostsWidget />
+            <CategoriesWidget />
+          </div>
+        </div>
+      </div>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 };
 
 export default Home;
