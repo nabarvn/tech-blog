b409a4665c473bf421c12bcacd6bdb359a50d554
Nabarun <nicknabarun@gmail.com>
2022-12-06 21:17:22 +0530

    Implement darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "#FAFAFA",
+        "night-black": "#0D1117",
+        "night-gray": "#21262D",
+        "night-teal": "#22F1BF",
+        "night-purple": "#3B2FCF",
+        "night-blue": "#4F51AF",
+        "night-white": "#FFFFFF",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp"), require("tailwind-scrollbar")],
+};
