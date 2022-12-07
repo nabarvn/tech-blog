e3af8b52e93f9e36f9366a8f9c5cd4f3c7562025
Nabarun <nicknabarun@gmail.com>
2022-12-07 20:43:40 +0530

    Implement NextNProgress

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,16 @@
 import Navbar from "./Navbar";
+import NextNProgress from "nextjs-progressbar";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <NextNProgress
+        color='linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248))'
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Navbar />
       {children}
     </>
