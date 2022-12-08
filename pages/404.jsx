728754b1e59bdb5c5565a0b613776f6d4c08874e
Nabarun <nicknabarun@gmail.com>
2022-12-09 00:09:37 +0530

    Change text styles

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ErrorPage = () => {
   return (
     <div className='flex flex-col m-auto justify-center items-center error-content'>
-      <div className='mb-8'>You seem lost, fren.</div>
+      <div className='text-2xl mb-8'>You seem lost, fren.</div>
       <Link href='/'>
         <button className='text-white dark:text-night-white bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-blue-900 rounded-lg font-semibold p-2'>
           Go Back Home?
