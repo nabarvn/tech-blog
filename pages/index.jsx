df6222ab2fc091de60dcc8aefa6a397240b0a60a
Nabarun <nicknabarun@gmail.com>
2023-01-30 22:34:47 +0530

    Minor fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,14 +23,14 @@ const Home = ({ posts }) => {
       </Head>
       <FeaturedPosts />
       <div className='grid grid-cols-1 md:grid-cols-12 gap-12 lg:gap-7 xl:gap-12'>
-        <div className='lg:grid lg:grid-cols-8 lg:gap-7 xl:gap-12 col-span-1 md:col-span-6 lg:col-span-8 xl:col-span-9'>
+        <div className='lg:grid lg:grid-cols-8 lg:gap-7 xl:gap-12 col-span-1 md:col-span-6 lg:col-span-9'>
           {posts.map((post) => (
             <div key={post.node.title} className='col-span-4'>
               <PostCard post={post.node} />
             </div>
           ))}
         </div>
-        <div className='col-span-1 md:col-span-6 lg:col-span-4 xl:col-span-3'>
+        <div className='col-span-1 md:col-span-6 lg:col-span-3'>
           <div className='relative md:sticky top-8'>
             <FeaturedPostsWidget />
             <PostsWidget />
