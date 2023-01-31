0db5c5133825f2fc03b5bb08155e0da44eb9f4fd
Nabarun <nicknabarun@gmail.com>
2023-01-31 20:12:36 +0530

    Minor change in code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,6 @@ import Link from "next/link";
 import { Dropdown } from ".";
 import ThemeSwitcher from "./ThemeSwitcher";
 
-// const categories = [
-//   { name: "Web 3.0", slug: "blockchain-engineering" },
-//   { name: "Web 2.0", slug: "web-development" },
-//   { name: "DSA", slug: "dsa" },
-// ];
-
 const Navbar = () => {
   return (
     <div className='container mx-auto px-9 lg:px-0 xl:px-9 mb-8'>
@@ -19,15 +13,7 @@ const Navbar = () => {
             </span>
           </Link>
         </div>
-        {/* <div className='hidden md:float-left md:contents'>
-          {categories.map((category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className='transition duration-300 transform hover:-translate-y-1 md:float-right mt-2 align-middle ml-6 font-semibold cursor-pointer'>
-                {category.name}
-              </span>
-            </Link>
-          ))}
-        </div> */}
+
         <div className='flex float-end'>
           <div className='self-center hidden md:contents'>
             <ThemeSwitcher />
