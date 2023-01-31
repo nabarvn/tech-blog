c0275444ec45484b4d904233087c2487f6583a0b
Nabarun <nicknabarun@gmail.com>
2023-01-31 20:35:44 +0530

    Refactor code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import { Dropdown } from ".";
 import ThemeSwitcher from "./ThemeSwitcher";
 
+// const categories = [
+//   { name: "Web 3.0", slug: "blockchain-engineering" },
+//   { name: "Web 2.0", slug: "web-development" },
+//   { name: "DSA", slug: "dsa" },
+// ];
+
 const Navbar = () => {
   return (
     <div className='container mx-auto px-9 lg:px-0 xl:px-9 mb-8'>
@@ -13,7 +19,15 @@ const Navbar = () => {
             </span>
           </Link>
         </div>
-
+        {/* <div className='hidden md:float-left md:contents'>
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className='transition duration-300 transform hover:-translate-y-1 md:float-right mt-2 align-middle ml-6 font-semibold cursor-pointer'>
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div> */}
         <div className='flex float-end'>
           <div className='self-center hidden md:contents'>
             <ThemeSwitcher />
