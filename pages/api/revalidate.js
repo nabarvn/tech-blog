f3206e92b7a1832cac11652fac77c7b044d7b689
Nabarun <nicknabarun@gmail.com>
2023-03-01 20:28:17 +0530

    Implement On-demand ISR

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,21 +1,34 @@
+// const handler = async (req, res) => {
+//   if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
+
+//   try {
+//     await res
+//       .revalidate("/")
+//       .then(res.revalidate("/category"))
+//       .then(res.revalidate("/post"))
+//       .then(res.revalidate("/tag"));
+
+//     return res.json({
+//       revalidated: true,
+//     });
+//   } catch (err) {
+//     return res.status(500).send("Error revalidating");
+//   }
+// };
+
+// export default handler;
+
 const handler = async (req, res) => {
-  if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+  await res.revalidate("/");
 
-  try {
-    await res
-      .revalidate("/")
-      .then(res.revalidate("/category"))
-      .then(res.revalidate("/post"))
-      .then(res.revalidate("/tag"));
+  const pathToRevalidate = `/${
+    req.body?.record?.id || req.body?.old_record?.id
+  }`;
+  await res.revalidate(pathToRevalidate);
 
-    return res.json({
-      revalidated: true,
-    });
-  } catch (err) {
-    return res.status(500).send("Error revalidating");
-  }
+  return res.send({ revalidated: true });
 };
 
 export default handler;
