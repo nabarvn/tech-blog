56a1ca4123818f841972198a616b556351dca5bf
Nabarun <nicknabarun@gmail.com>
2023-03-01 22:18:46 +0530

    Implement On-demand ISR

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,29 +1,9 @@
-// const handler = async (req, res) => {
-//   if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
-//     return res.status(401).json({ message: "Invalid token" });
-//   }
-
-//   try {
-//     await res
-//       .revalidate("/")
-//       .then(res.revalidate("/category"))
-//       .then(res.revalidate("/post"))
-//       .then(res.revalidate("/tag"));
-
-//     return res.json({
-//       revalidated: true,
-//     });
-//   } catch (err) {
-//     return res.status(500).send("Error revalidating");
-//   }
-// };
-
 const handler = async (req, res) => {
   const pathsToRevalidate = [
     "/",
     `/post/${req.body.data.slug}`,
-    "/category",
-    "/tag",
+    `/category/${req.body.data.categories}`,
+    `/tag/${req.body.data.tags}`,
   ];
 
   // Check for secret to confirm this is a valid request
