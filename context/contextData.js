854c854592cee290291b36817475bae8d0259c3a
Nabarun <nicknabarun@gmail.com>
2023-03-02 19:42:39 +0530

    Initial commit

---
+++ context/contextData.js
@@ -0,0 +1,32 @@
+import { useEffect, useState, createContext } from "react";
+import { getCategories, getFeaturedPosts, getTags } from "../services";
+
+const appContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+  const [featuredPosts, setFeaturedPosts] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getFeaturedPosts().then((res) => {
+      setFeaturedPosts(res);
+    });
+
+    getTags().then((res) => {
+      setTags(res);
+    });
+
+    getCategories().then((res) => {
+      setCategories(res);
+    });
+  }, []);
+
+  return (
+    <appContext.Provider value={{ featuredPosts, tags, categories }}>
+      {children}
+    </appContext.Provider>
+  );
+};
+
+export default appContext;
