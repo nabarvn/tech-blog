1e4be000b5ab13b3616613fe7f9c7eeca8317efe
Nabarun <nicknabarun@gmail.com>
2023-03-05 00:11:27 +0530

    Test

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -236,153 +236,16 @@ export default async function handler(req, res) {
   let pathsToRevalidate = [];
 
   if (action === "update" && model === "Post") {
-    // const categories = req.body.data.categories;
-    // const tags = req.body.data.tags;
-
-    // categories.forEach((category) => {
-    //   if (category.id === "clab85xm32ptn0appmpdm4znu") {
-    //     tags.forEach((tag) => {
-    //       if (tag.id === "clb59ix6d0pjv0apmxtm7ht6s") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/dsa`,
-    //           `/tag/react`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "clb59u4rd0prn0apmkr7tnqgf") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/dsa`,
-    //           `/tag/solidity`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "clb5hv0hf0wua0bo8j9cqwebn") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/dsa`,
-    //           `/tag/web3`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "cleirgspm04qt0bpk4arkowp4") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/dsa`,
-    //           `/tag/dsa`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-    //     });
-    //   }
-
-    //   if (category.id === "clab87n4x2pyt0appgdq0n4mu") {
-    //     tags.forEach((tag) => {
-    //       if (tag.id === "clb59ix6d0pjv0apmxtm7ht6s") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/web-development`,
-    //           `/tag/react`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "clb59u4rd0prn0apmkr7tnqgf") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/web-development`,
-    //           `/tag/solidity`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "clb5hv0hf0wua0bo8j9cqwebn") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/web-development`,
-    //           `/tag/web3`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "cleirgspm04qt0bpk4arkowp4") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/web-development`,
-    //           `/tag/dsa`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-    //     });
-    //   }
-
-    //   if (category.id === "clab8az412q0x0app8jqpg0yx") {
-    //     tags.forEach((tag) => {
-    //       if (tag.id === "clb59ix6d0pjv0apmxtm7ht6s") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/blockchain-engineering`,
-    //           `/tag/react`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "clb59u4rd0prn0apmkr7tnqgf") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/blockchain-engineering`,
-    //           `/tag/solidity`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "clb5hv0hf0wua0bo8j9cqwebn") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/blockchain-engineering`,
-    //           `/tag/web3`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-
-    //       if (tag.id === "cleirgspm04qt0bpk4arkowp4") {
-    //         pathsToRevalidate = [
-    //           `/`,
-    //           `/post/${slug}`,
-    //           `/category/blockchain-engineering`,
-    //           `/tag/dsa`,
-    //           ...pathsToRevalidate,
-    //         ];
-    //       }
-    //     });
-    //   }
-    // });
-
     pathsToRevalidate = [
       `/`,
       `/post/${slug}`,
-      `/category/dsa`,
-      `/category/web-development`,
-      `/category/blockchain-engineering`,
       `/tag/react`,
       `/tag/solidity`,
       `/tag/web3`,
       `/tag/dsa`,
+      `/category/dsa`,
+      `/category/web-development`,
+      `/category/blockchain-engineering`,
     ];
   } else {
     switch (model) {
