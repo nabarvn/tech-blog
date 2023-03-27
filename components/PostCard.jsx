623d28b3645bbc30b962359aa17f65a51ceae14d
Nabarun <nicknabarun@gmail.com>
2023-03-27 16:05:12 +0530

    Fix styles

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const PostCard = ({ post }) => {
   return (
-    <div className='flex flex-col bg-white dark:bg-night-gray shadow-lg group transition duration-300 dark:shadow-md hover:shadow-2xl dark:hover:shadow-night-teal rounded-lg p-0 lg:p-5 pb-3 mb-8 lg:mb-0 hover:cursor-pointer'>
+    <div className='flex flex-col bg-white dark:bg-night-gray shadow-lg group transition duration-300 dark:shadow-md hover:shadow-2xl hover:shadow-blue-500 dark:hover:shadow-night-teal rounded-lg p-0 lg:p-5 pb-3 mb-8 lg:mb-0 hover:cursor-pointer'>
       <Link href={`/post/${post.slug}`}>
         <div className='relative overflow-hidden md:shadow-md pb-48 mb-8'>
           <img
@@ -12,7 +12,7 @@ const PostCard = ({ post }) => {
             className='absolute h-48 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
           />
         </div>
-        <h1 className='transition duration-300 text-center mb-8 cursor-pointer dark:text-night-white hover:text-blue-500 dark:group-hover:text-night-teal text-xl md:text-2xl font-semibold px-1 lg:px-0'>
+        <h1 className='transition duration-300 text-center mb-8 cursor-pointer dark:text-night-white group-hover:text-blue-500 dark:group-hover:text-night-teal text-xl md:text-2xl font-semibold px-1 lg:px-0'>
           {/* <Link href={`/post/${post.slug}`}>{post.title}</Link> */}
           {post.title}
         </h1>
